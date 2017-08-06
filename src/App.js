0b31292353c43864d23624be85dda4873b3bf09c
Andres Aguilar <andres.i.aguilar.a@gmail.com>
2017-08-05 20:07:57 -0400

    Adding React-Router
    Adding React-Router and update the code, now the app can navigate

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchPage from './SearchPage'
 import BookList from './BookList'
+import {Route} from 'react-router-dom'
 class BooksApp extends React.Component {
   state = {
     /**
@@ -24,11 +25,8 @@ class BooksApp extends React.Component {
 
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchPage/>
-        ) : (
-          <BookList/>
-        )}
+        <Route exact path="/" component={BookList} />
+        <Route path="/search" component={SearchPage} />
       </div>
     )
   }
