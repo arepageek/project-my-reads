0095fb9dc7bf4f847f32153709ed576bbdbc829e
Andres Aguilar <andres.i.aguilar.a@gmail.com>
2017-08-06 16:07:07 -0400

    Final
    Final App

--- src/App.js
+++ src/App.js
@@ -6,27 +6,79 @@ import BookList from './BookList'
 import {Route} from 'react-router-dom'
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: [], //Save books!
+    books_search: [], //Save Searched books!
+    query: '' //Save search query
   }
 
   componentDidMount(){
+    //Get all books from API
     BooksAPI.getAll().then((books) => {
-      books.map((book) => console.log(book.shelf) )
+      this.setState({books})
     });
   }
+
+  updateBook = (book,status) =>{
+    //First change the book shelf to new status
+      book.shelf = status
+      //First filter the book to delete the book from list and then add again the book with the new shelf
+      this.setState((state) => ({
+        books: [book,...state.books.filter((c) => c.id !==book.id)]
+      }))
+      //Update the book api
+    BooksAPI.update(book,status).then(() => {
+    });
+  }
+
+  searchBook = (query) => {
+
+    this.setState({query})
+    //If there is no query, empty books_search
+    if(!this.state.query){
+      this.setState({books_search:[]})
+    }
+    //Search Book on api
+        BooksAPI.search(query,4).then((books_search) => {
+          // Books sometimes return error... so i filter
+          if(!books_search.error){
+            //I search the searched book on the books and update shelf
+            books_search.map(book_search => 
+              this.state.books.map(book => {
+                if(book_search.id === book.id){
+                  book_search.shelf= book.shelf
+                }
+              }
+              )
+            )
+            //Update books_search
+            this.setState({books_search:books_search})
+          }else{
+            this.setState({books_search:[]})
+          }
+        }
+      
+        )
+      
+   }
 
   render() {
 
     return (
       <div className="app">
-        <Route exact path="/" component={BookList} />
-        <Route path="/search" component={SearchPage} />
+        <Route exact path="/" render={()=>
+          <BookList
+            bookList={this.state.books} 
+            updateBook={this.updateBook}
+
+          />
+        }/>
+        <Route path="/search" render={()=>
+          <SearchPage
+            bookList={this.state.books_search} 
+            updateBook={this.updateBook}
+            searchBook={this.searchBook}
+            querySearch={this.state.query}
+          />} />
       </div>
     )
   }
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,27 +1,58 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import {PropTypes} from 'prop-types'
+
 
 class SearchPage extends Component {
+    static propTypes= {
+        bookList: PropTypes.array.isRequired,
+        updateBook: PropTypes.func.isRequired,
+        searchBook: PropTypes.func.isRequired,
+        query: PropTypes.string.isRequired
+
+    }
     render() {
+        const {bookList,updateBook,searchBook,querySearch} = this.props
+        console.log(bookList)
         return (
-<div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/* 
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-                
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link to='/' className="close-search" >Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <input type="text" placeholder="Search by title or author" value={querySearch} onChange={(event) => searchBook(event.target.value)
+                        }/>             
+                    </div>
+                </div>
+                <div className="search-books-results">
+                <ol className="books-grid">
+                {
+                    bookList.length !== 0 && (
+                    bookList.map((book) => 
+                      <li key={book.id}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ 
+                                width: 128, 
+                                height: 193, 
+                                backgroundImage: `url(${book.imageLinks.thumbnail})`
+                             }}></div>
+                            <div className="book-shelf-changer">
+                              <select value={book.shelf} onChange={(event)=> updateBook(book,event.target.value)}>
+                                <option value="disabled" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                       <div className="book-authors">{book.authors}</div>
+                        </div>
+                      </li>
+                    ))}
+                </ol>
+                </div>
           </div>
         );
     }
